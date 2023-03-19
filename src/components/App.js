cb5df00cc2656be7d83864d94bcf0abec44dfa68
gdwb3219 <gdwb3219@gmail.com>
2023-03-19 23:49:54 +0900

    MoviePedia 2023 first commit

---
+++ src/components/App.js
@@ -0,0 +1,24 @@
+import ReviewList from './ReviewList';
+import items from '../mock.json';
+import { useState } from 'react';
+
+function App() {
+  const [order, setOrder] = useState('createdAt');
+  const sortedItems = items.sort((a, b) => b[order] - a[order]);
+
+  const handleNewestClick = () => setOrder('createdAt');
+
+  const handleRatingClick = () => setOrder('rating');
+
+  return (
+    <div>
+      <div>
+        <button onClick={handleNewestClick}>최신순</button>
+        <button onClick={handleRatingClick}>별점순</button>
+      </div>
+      <ReviewList items={sortedItems} />
+    </div>
+  );
+}
+
+export default App;
