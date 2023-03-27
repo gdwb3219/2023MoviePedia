24a88acd010e2df183f802ced511bc37cd9e9aea
gdwb3219 <gdwb3219@gmail.com>
2023-03-27 23:56:09 +0900

    add Delete function

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,20 @@
 import ReviewList from './ReviewList';
-import items from '../mock.json';
+import mockItems from '../mock.json';
 import { useState } from 'react';
 
 function App() {
   const [order, setOrder] = useState('createdAt');
+  const [items, setItems] = useState(mockItems);
+
   const sortedItems = items.sort((a, b) => b[order] - a[order]);
 
   const handleNewestClick = () => setOrder('createdAt');
-
   const handleRatingClick = () => setOrder('rating');
+
+  const handleDelete = (id) => {
+    const nextItems = items.filter((item) => item.id !== id);
+    setItems(nextItems);
+  };
 
   return (
     <div>
@@ -16,7 +22,7 @@ function App() {
         <button onClick={handleNewestClick}>최신순</button>
         <button onClick={handleRatingClick}>별점순</button>
       </div>
-      <ReviewList items={sortedItems} />
+      <ReviewList items={sortedItems} onDelete={handleDelete} />
     </div>
   );
 }
