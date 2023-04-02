0ca49f69feaa842823484e521cad16d6dddfc1ee
gdwb3219 <gdwb3219@gmail.com>
2023-04-02 23:20:29 +0900

    Understanding about useEffect Hook

--- src/api.js
+++ src/api.js
@@ -1,7 +1,10 @@
 // 이 파일에다가 네트워크 리퀘스트 함수들을 모아놓고 사용
 
-export async function getReviews() {
-  const response = await fetch('https://learn.codeit.kr/api/film-reviews');
+export async function getReviews(order = 'createdAt') {
+  const query = `order=${order}`;
+  const response = await fetch(
+    `https://learn.codeit.kr/api/film-reviews?${query}`
+  );
   const body = await response.json();
   return body;
 }
