7401ac78a42d1bce3d153ea17cf03ec48ca4a2f6
gdwb3219 <gdwb3219@gmail.com>
2023-04-04 23:51:44 +0900

    Learn pagination

--- src/api.js
+++ src/api.js
@@ -1,6 +1,10 @@
 // 이 파일에다가 네트워크 리퀘스트 함수들을 모아놓고 사용
 
-export async function getReviews(order = 'createdAt') {
+export async function getReviews({
+  order = 'createdAt',
+  offset = 0,
+  limit = 6,
+}) {
   const query = `order=${order}`;
   const response = await fetch(
     `https://learn.codeit.kr/api/film-reviews?${query}`
