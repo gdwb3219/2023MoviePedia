2a0695ea02eae6d5f3e352911e95ef37c09a970a
gdwb3219 <gdwb3219@gmail.com>
2023-05-02 23:36:35 +0900

    Add Preview State

--- src/components/FilesInput.js
+++ src/components/FilesInput.js
@@ -1,7 +1,10 @@
-import { useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 function FilesInput({ name, value, onChange }) {
+  const [preview, setPreiview] = useState();
+
   const inputRef = useRef();
+
   const handleChange = (e) => {
     const nextValue = e.target.files[0];
     onChange(name, nextValue);
@@ -14,8 +17,15 @@ function FilesInput({ name, value, onChange }) {
     onChange(name, null);
   };
 
+  useEffect(() => {
+    if (!value) return;
+    const nextPreview = URL.createObjectURL(value);
+    setPreiview(nextPreview);
+  }, [value]);
+
   return (
     <div>
+      <img src={preview} alt="이미지 미리보기" />
       <input type="file" onChange={handleChange} ref={inputRef} />
       {value && <button onClick={handleClearClick}>X</button>}
     </div>
